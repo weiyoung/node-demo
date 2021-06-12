ce66f711fa250a26fcca5dc7dee8d003ee7c5a16
Weiyoung Tan <wtan@bgcengineering.ca>
2021-06-12 13:28:52 -0700

    get request

--- demo-express-app/routes/users.js
+++ demo-express-app/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [
+  {
+    "username": "dgipps",
+    "first_name": "Daniel",
+    "last_name": "Gipps"
+  }
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(users);
+});
+
+router.post('/',  function(req, res, next) {
+  const newUser = req.body;
+
+  users.push(newUser);
+
+  res.send(newUser);
 });
 
 module.exports = router;
